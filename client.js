cda9fcd35d13a86d1027061e0fcbb6392c0b6f25
Vignesh <destanvignesh7@gmail.com>
2019-07-17 17:21:05 +0530

    feature2

--- client.js
+++ client.js
@@ -1,9 +1,9 @@
 var express = require('Express');
+//app
 var app = express();
 var clientServer = require('http').createServer(app);
 clientServer.listen(8080);
 app.use(express.static("public"));
-//get function
 app.get('/index.html',function(req,res){
 	res.sendfile('index.html');
 });
