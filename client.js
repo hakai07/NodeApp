1ba342ee2eb2543c486bdde7462305b66ca36760
Vignesh <destanvignesh7@gmail.com>
2019-07-17 16:35:01 +0530

    Feature1

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ var clientServer = require('http').createServer(app);
 clientServer.listen(8080);
 app.use(express.static("public"));
 
+//get function
 app.get('/',function(req,res){
 	res.sendfile('index.html');
 });
